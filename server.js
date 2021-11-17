4288456aa3f961452e4fe39689105806b5b53ab4
rahul <rahuldhillon27@gmail.com>
2021-11-18 04:57:13 +0530

    using path to sendFile

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
 const express = require("express");
 const app = express();
 
-// Our port is hardcoded below, if port is unavaiable then we have a prob.
-// So we will create a env variable and use 3000 as fall back
-// We can setup PORT in terminal PORT = 5000 node server
+// to generate path, to use different files like index.html
+const path = require("path");
 
 const PORT = process.env.PORT || 3000;
 
-//fixing cannot GET/
-// for get req for root and pass second parameter
-// for req and res
+// but we never send inline html
+// hence create a file
+// use path to senFile
 app.get("/", (req, res) => {
-  res.send("<h1>Hello from Express..,</h1>");
+  res.sendFile(path.resolve(__dirname + "/index.html"));
 });
 
 app.listen(PORT, () => {
