5ee909e3e5822d22ab4313abc06f292009ce0c9a
rahul <rahuldhillon27@gmail.com>
2021-11-18 04:34:02 +0530

    Cannot GET /

--- server.js
+++ server.js
@@ -4,5 +4,6 @@ const express = require("express");
 const app = express();
 
 //first method we use is listen
-// run by node server, server will start
+//run by node server, server will start
+// '/' means homepage, and will print cannot GET /
 app.listen(3000);
