74a8483417f7053eec0fc707b77b913395a4ba3b
rahul <rahuldhillon27@gmail.com>
2021-11-18 05:00:08 +0530

    create multiple pages

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 const express = require("express");
 const app = express();
 
-// to generate path, to use different files like index.html
 const path = require("path");
 
 const PORT = process.env.PORT || 3000;
 
-// but we never send inline html
-// hence create a file
-// use path to senFile
+// Now to generate multiple pages for our Website
+// We will create new routes
+
 app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname + "/index.html"));
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(path.resolve(__dirname + "/about.html"));
 });
 
 app.listen(PORT, () => {
