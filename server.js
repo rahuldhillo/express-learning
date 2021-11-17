0f7f70a144bfd27dfeaf4043638aeceaf170a375
rahul <rahuldhillon27@gmail.com>
2021-11-18 04:32:21 +0530

    node server - start server

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+
+//below object will have functions
+const app = express();
+
+//first method we use is listen
+// run by node server, server will start
+app.listen(3000);
