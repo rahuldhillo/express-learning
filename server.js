cb1baecb82cfa67e90dadc46a38089659171e8bc
rahul <rahuldhillon27@gmail.com>
2021-11-18 04:47:48 +0530

    fix cannot GET - setting get res req

--- server.js
+++ server.js
@@ -3,8 +3,16 @@ const app = express();
 
 // Our port is hardcoded below, if port is unavaiable then we have a prob.
 // So we will create a env variable and use 3000 as fall back
+// We can setup PORT in terminal PORT = 5000 node server
 
 const PORT = process.env.PORT || 3000;
+
+//fixing cannot GET/
+// for get req for root and pass second parameter
+// for req and res
+app.get("/", (req, res) => {
+  res.send("Hello from Express");
+});
 
 app.listen(PORT, () => {
   console.log(`server is running on PORT ${PORT}`);
