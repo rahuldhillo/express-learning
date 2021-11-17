88c021112fc2c54a5e19285c47915f6cddbc145d
rahul <rahuldhillon27@gmail.com>
2021-11-18 04:36:11 +0530

    callback func() as second parameter

--- server.js
+++ server.js
@@ -6,4 +6,8 @@ const app = express();
 //first method we use is listen
 //run by node server, server will start
 // '/' means homepage, and will print cannot GET /
-app.listen(3000);
+// add a call back function, as a second parameter,
+// this will run after server will start
+app.listen(3000, () => {
+  console.log("server is running");
+});
