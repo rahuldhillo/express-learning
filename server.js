cbd7df1cebd368f230c4645a9b922c82422b22bc
rahul <rahuldhillon27@gmail.com>
2021-11-18 05:54:22 +0530

    using render for views

--- server.js
+++ server.js
@@ -5,16 +5,17 @@ const PORT = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 
-// We can rename the views folder by app.set
-app.set("views", path.resolve(__dirname) + "/templates");
-console.log(app.get("views"));
-
 app.use(express.static("public"));
+
+// now instead of sendFile we will use render
+// render will render the view/ templates
+// no need to use .ejs extension just pass the name
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname + "/index.html"));
+  res.render("index");
 });
+
 app.get("/about", (req, res) => {
-  res.sendFile(path.resolve(__dirname + "/about.html"));
+  res.render("about");
 });
 app.get("/download", (req, res) => {
   res.download(path.resolve(__dirname + "/about.html"));
