9463a3122c33c852c4d2550ecdfce6a712fcced9
rahul <rahuldhillon27@gmail.com>
2021-11-18 05:41:17 +0530

    set and get in express - view engine

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const app = express();
-
 const path = require("path");
-
 const PORT = process.env.PORT || 3000;
+
+// tell express that we want to use something in this case view engine
+// app.set
+// set method accepts key value and then we can use it by get
+
+app.set("view engine", "ejs");
+
+console.log(app.get("view engine"));
 
 app.use(express.static("public"));
 
@@ -15,8 +21,6 @@ app.get("/about", (req, res) => {
   res.sendFile(path.resolve(__dirname + "/about.html"));
 });
 
-// We can download file
-// res.download easy peasy
 app.get("/download", (req, res) => {
   res.download(path.resolve(__dirname + "/about.html"));
 });
@@ -24,3 +28,6 @@ app.get("/download", (req, res) => {
 app.listen(PORT, () => {
   console.log(`server is running on PORT ${PORT}`);
 });
+
+// Now we will use template engine - pug, handlebar, ejs
+// We will use ejs for dynamic pages
