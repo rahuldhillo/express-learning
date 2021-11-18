7ea6baeca5b9600b19f6c729a63a66899a088cb0
rahul <rahuldhillon27@gmail.com>
2021-11-18 05:48:22 +0530

    renaming views folder

--- server.js
+++ server.js
@@ -3,36 +3,22 @@ const app = express();
 const path = require("path");
 const PORT = process.env.PORT || 3000;
 
-// tell express that we want to use something in this case view engine
-// app.set
-// set method accepts key value and then we can use it by get
-
 app.set("view engine", "ejs");
 
-// Below will print ejs
-// console.log(app.get("view engine"));
-
-// Below means that template engine will search in this folder
-// So we need to create a views folder
+// We can rename the views folder by app.set
+app.set("views", path.resolve(__dirname) + "/templates");
 console.log(app.get("views"));
 
 app.use(express.static("public"));
-
 app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname + "/index.html"));
 });
-
 app.get("/about", (req, res) => {
   res.sendFile(path.resolve(__dirname + "/about.html"));
 });
-
 app.get("/download", (req, res) => {
   res.download(path.resolve(__dirname + "/about.html"));
 });
-
 app.listen(PORT, () => {
   console.log(`server is running on PORT ${PORT}`);
 });
-
-// Now we will use template engine - pug, handlebar, ejs
-// We will use ejs for dynamic pages
