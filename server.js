d2515958bccf1183375c5866611f645319fa3be3
rahul <rahuldhillon27@gmail.com>
2021-11-18 06:56:22 +0530

    using first parameter in router

--- server.js
+++ server.js
@@ -8,12 +8,9 @@ app.set("view engine", "ejs");
 
 app.use(express.static("public"));
 
-// to maintain multiple pages use router
-// we can create seprate files for different routes
-// but can create an index.js for small projects
-// We have to tell server file that we have routes
+// we can use first parameter as prefix
 
-app.use(mainRouter);
+app.use("/en", mainRouter);
 
 app.listen(PORT, () => {
   console.log(`server is running on PORT ${PORT}`);
