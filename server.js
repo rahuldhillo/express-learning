df049c6e1ef980cccad58ce779721fe56dd9bad2
rahul <rahuldhillon27@gmail.com>
2021-11-18 05:35:11 +0530

    using res.download

--- server.js
+++ server.js
@@ -5,14 +5,21 @@ const path = require("path");
 
 const PORT = process.env.PORT || 3000;
 
-// in public folder the index.html is by default acting as default file
-// it is a convention
-// So static content will load index file by default
-// we can change about.html in browser path but for now we can use route
-// We cankeep CSS and image file in public forlder as static content
-// and then include in out html by simply using /path
-
 app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname + "/index.html"));
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(path.resolve(__dirname + "/about.html"));
+});
+
+// We can download file
+// res.download easy peasy
+app.get("/download", (req, res) => {
+  res.download(path.resolve(__dirname + "/about.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`server is running on PORT ${PORT}`);
