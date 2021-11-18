9efd090213a4e5c13e024f758e3dc9e0b191e175
rahul <rahuldhillon27@gmail.com>
2021-11-18 07:21:03 +0530

    using middleware - api_key - req.query

--- server.js
+++ server.js
@@ -10,13 +10,6 @@ app.use(express.static("public"));
 
 app.use(mainRouter);
 
-// Designining custom middleware
-// We will simulate a req
-// send dummy api key and check key to process req
-// else send message to log not valid
-// create route for path like /api/products?api_key=1234567
-// create a seprate middlware folder
-
 app.listen(PORT, () => {
   console.log(`server is running on PORT ${PORT}`);
 });
