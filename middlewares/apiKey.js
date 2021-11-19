478343428b47339981c159fa43a3763eb6b3f4ff
rahul <rahuldhillon27@gmail.com>
2021-11-19 05:37:03 +0530

    router level middleware

--- middlewares/apiKey.js
+++ middlewares/apiKey.js
@@ -1,6 +1,7 @@
 function apiKey(req, res, next) {
   const api_key = "1234567";
   console.log(req.query.api_key);
+  const userApiKey = req.query.api_key;
 
   if (userApiKey && userApiKey === api_key) {
     next();
