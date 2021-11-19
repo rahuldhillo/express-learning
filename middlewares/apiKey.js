d037aa32a2510f48d154286e125cbd31bc9c20d4
rahul <rahuldhillon27@gmail.com>
2021-11-19 05:32:34 +0530

    checking api key - route based middleware

--- middlewares/apiKey.js
+++ middlewares/apiKey.js
@@ -1,8 +1,18 @@
 function apiKey(req, res, next) {
   const api_key = "1234567";
   //console.log(req.body); //only with post data
-  console.log(req.query); // for query bcs we are sending query
+  console.log(req.query.api_key); // for query bcs we are sending query
   // all the middleware between req and res are in a line so we have to call next
+  // if we are not calling next then it means requests are not going forward.
+
+  // Middleware is useful for authorization, token key check, user roles, permissions
+  const userApiKey = req.query.api_key;
+  if (userApiKey && userApiKey === api_key) {
+    next();
+  } else {
+    res.json({ message: "Not Allowed!" });
+  }
+
   next();
 }
 
