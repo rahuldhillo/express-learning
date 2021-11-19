44784b4840329c8a6a901d05dacb51525aae545d
rahul <rahuldhillon27@gmail.com>
2021-11-19 05:44:03 +0530

    global middleware - v3

--- server.js
+++ server.js
@@ -3,10 +3,14 @@ const app = express();
 const path = require("path");
 const PORT = process.env.PORT || 3000;
 const mainRouter = require("./routes/index");
+const apiKeyMiddleware = require("./middlewares/apiKey");
+
+app.use(apiKeyMiddleware);
 
 app.set("view engine", "ejs");
 
 //Global middleware, we write app.use instead of router.use
+
 app.use(express.static("public"));
 
 app.use(mainRouter);
